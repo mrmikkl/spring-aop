a5c35bdb33c8cb20fba510ef0565df2accbfcde1
Gödecke, Maik <maik.goedecke@god.de>
2018-08-03 15:07:38 +0200

    small test changes

--- src/main/java/de/god/usecase/AopUseCase.java
+++ src/main/java/de/god/usecase/AopUseCase.java
@@ -1,5 +1,6 @@
 package de.god.usecase;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import javax.validation.constraints.NotNull;
@@ -9,6 +10,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class AopUseCase
 {
+    private String innerState = "";
+
     public AopUseCase()
     {
     }
@@ -22,5 +25,50 @@ public class AopUseCase
     {
         number += 1;
         return number;
+    }
+
+    public String changeState(@NotNull String... values)
+    {
+        final StateBuilder stateBuilder = new StateBuilder(this.innerState);
+        Arrays.stream(values).forEach(value -> stateBuilder.addValue(value));
+        return stateBuilder.build();
+    }
+
+    public String changeTripleState(@NotNull String value1, @NotNull String value2, @NotNull String value3)
+    {
+        final StateBuilder stateBuilder = new StateBuilder(this.innerState);
+        return stateBuilder
+                .addValue(value1)
+                .addValue(value2)
+                .addValue(value3)
+                .build();
+    }
+
+    public String getInnerState()
+    {
+        return innerState;
+    }
+
+    public class StateBuilder
+    {
+        private String state;
+        private String value;
+
+        public StateBuilder(final String state)
+        {
+            this.state = state;
+        }
+
+        public StateBuilder addValue(final String value)
+        {
+            this.value += value;
+            return this;
+        }
+
+        public String build()
+        {
+            this.state += this.value;
+            return this.state;
+        }
     }
 }
--- src/test/java/de/god/usecase/AopUseCaseTest.java
+++ src/test/java/de/god/usecase/AopUseCaseTest.java
@@ -1,24 +1,33 @@
 package de.god.usecase;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.UUID;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+@DisplayName("The AOP UseCase")
 @RunWith(MockitoJUnitRunner.class)
 class AopUseCaseTest
 {
-//    @Mock
-//    private AopUseCase aopUseCase;
+    //    @Mock
+    AopUseCase aopUseCase;
 
     @BeforeEach
     void setUp()
     {
+        this.aopUseCase = Mockito.mock(AopUseCase.class);
     }
 
     @AfterEach
@@ -26,11 +35,10 @@ class AopUseCaseTest
     {
     }
 
-    @DisplayName("Tests to add 1 to 1.")
+    @DisplayName("is used to add 1 to a number")
     @Test
-    public void addOne()
+    void addOne()
     {
-        final AopUseCase aopUseCase = Mockito.mock(AopUseCase.class);
         Mockito.when(aopUseCase.addOne(1L)).thenReturn(2L);
 
         Long testNumber = 1L;
@@ -40,14 +48,51 @@ class AopUseCaseTest
         Mockito.verify(aopUseCase).addOne(testNumber);
     }
 
-    @DisplayName("Tests to return a random generated UUID.")
+    @DisplayName("is used to return a random UUID")
     @Test
-    public void getRandomUUID()
+    void getRandomUUID()
     {
         final AopUseCase aopUseCase = Mockito.mock(AopUseCase.class);
         Mockito.when(aopUseCase.getRandomUUID()).thenReturn(UUID.randomUUID().toString());
 
         final String uuid = aopUseCase.getRandomUUID();
         Mockito.verify(aopUseCase).getRandomUUID();
+    }
+
+    @Nested
+    @DisplayName("with new state")
+    class NewState
+    {
+        private String state;
+
+        @BeforeEach
+        void setUp()
+        {
+            this.state = "newState";
+        }
+
+        @DisplayName("has a changed state")
+        @Test
+        void changeState()
+        {
+            final String stateChange1 = "1";
+            final String stateChange2 = "2";
+            final String stateChange3 = "3";
+            Mockito.when(aopUseCase.changeTripleState(stateChange1, stateChange2, stateChange3)).thenReturn(this.state + "123");
+
+            final String changedState = aopUseCase.changeTripleState(stateChange1, stateChange2, stateChange3);
+            assertEquals(changedState, this.state + "123");
+        }
+
+        @DisplayName("can be spied on")
+        @Test
+        void spying(){
+            final List<String> stringList = Arrays.asList("One", "Two", "Three");
+            final List<String> stringListSpy = Mockito.spy(stringList);
+
+            Mockito.when(aopUseCase.changeState("Four")).thenReturn("OneTwoThreeFour");
+
+            
+        }
     }
 }
